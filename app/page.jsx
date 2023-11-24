c03c84999b36a76b327369b504c8d335a5cdc08c
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2023-11-24 14:11:39 -0300

    Add Minecraft and Title-Minecraft fonts

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,13 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <h1 className={styles.teste}>
-      home
+    <>
+    <h1 className={styles.teste_com_titulo}>
+      MINECRAFT
     </h1>
+    <p className={styles.teste}>
+      Apenas um teste de fonte
+    </p>
+    </>
   )
 }
