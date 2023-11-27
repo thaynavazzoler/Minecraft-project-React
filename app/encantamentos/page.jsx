1e5dfd923c82da8cd1bdca8f097de6ff91f8f6b0
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2023-11-27 08:49:08 -0300

    Refactor encantamentos page layout and add new images

--- app/encantamentos/page.jsx
+++ app/encantamentos/page.jsx
@@ -7,36 +7,26 @@ import styles from './page.module.css';
 const Page_de_encantamentos = () => {
     return (
         <main className={styles.main}>
-
-            <div className={styles.main_container}>
-                <h1 className={styles.titles}>Encantamentos</h1> <h2 className={styles.sigilos_subTitles}>Encantamentos</h2>
-                <p>Encantamentos são habilidades que podem ser adicionadas a armas e armaduras para melhorar suas características. Existem 3 tipos de encantamentos: Encantamentos de armadura, Encantamentos de armas e Encantamentos de ferramentas.
-                    Alem disso existem encantamentos que podem ser aplicados em qualquer item, como o encantamento Maldição do ligamento, que impede que o item seja retirado do jogador, ou o encantamento Maldição do desaparecimento, que faz com que o item desapareça ao morrer.</p>
-
-                <h2>Encantamentos de armadura</h2>
-                <p>Os encantamentos de armadura são encantamentos que podem ser aplicados em armaduras. Eles são divididos em 4 categorias: Encantamentos de capacete, Encantamentos de peitoral, Encantamentos de calças e Encantamentos de botas.</p>
+            <div className={styles.container_text}>
+                <h1 className={styles.titles}>ENCANTAMENTOS</h1> <h2 className={styles.subTitles}>ENCANTAMENTOS</h2>
+                <nav className={styles.encantamentos}>
+                    <ul className={styles.lista_encantamentos}>
+                        <li className={styles.encantamento}>
+                            <img className={styles.book} src="/Enchanted_Book.webp" alt="encantamento" />
+                            <h2 className={styles.nome_encantamento}> Afiacao</h2>
+                        </li>
+                        <li className={styles.encantamento}>
+                            <img className={styles.book} src="/Enchanted_Book.webp" alt="encantamento" />
+                            <h2 className={styles.nome_encantamento}> protecao</h2>
+                        </li>
+                        <li className={styles.encantamento}>
+                            <img className={styles.book} src="/Enchanted_Book.webp" alt="encantamento" />
+                            <h2 className={styles.nome_encantamento}> Aspecto de fogo</h2>
+                        </li>
+                        
+                    </ul>
+                </nav>
             </div>
-
-
-                    <nav className={styles.books_container}>
-
-
-                        <ul className={styles.encantamentos_container}>
-                            <li className={styles.encantamentos}>
-                                <img className={styles.encantamentos_img} src="/images/encantamentos/encantamentos_de_armadura/encantamento_de_protecao.png" alt="encantamento de proteção" />
-                                <p className={styles.encantamentos_name}>Protecao</p>
-                            </li>
-                            <li className={styles.encantamentos}>
-                                <img className={styles.encantamentos_img} src="/images/encantamentos/encantamentos_de_armadura/encantamento_de_respiracao.png" alt="encantamento de respiração" />
-                                <p className={styles.encantamentos_name}>Respiracao</p>
-                            </li>
-                            <li className={styles.encantamentos}>
-                                <img className={styles.encantamentos_img} src="/images/encantamentos/encantamentos_de_armadura/encantamento_de_respiracao_aquatica.png" alt="encantamento de respiração aquatica" />
-                                <p className={styles.encantamentos_name}>Respiraco aquatica</p>
-                            </li>
-                        </ul>
-                 
-                    </nav>
         </main>
 
     )
