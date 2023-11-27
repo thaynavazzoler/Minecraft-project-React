c4094ba54a2d959ff58c6b9858093716ed030e42
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2023-11-27 15:39:51 -0300

    Refactor encantamentos page layout

--- app/encantamentos/page.jsx
+++ app/encantamentos/page.jsx
@@ -8,36 +8,46 @@ import styles from './page.module.css';
 const Page_de_encantamentos = () => {
     return (
         <main className={styles.main}>
-            <Header />
-            <h1 className={styles.titles}>Encantamentos</h1>
-            <h2 className={styles.subTitles}>Encantamentos</h2>
+            {/* <Header /> */}
+            <div className={styles.titles_container}>
+                <h1 className={styles.titles}>Encantamentos</h1>
+                <h2 className={styles.subTitles}>Encantamentos</h2>
+            </div>
             <div className={styles.main_container}>
                 <nav className={styles.encantamentos_container}>
                     <ul className={styles.encantamentos_list}>
+                        Centro
                         <li className={styles.encantamento}>
-                            <h2>
-                                <span className={styles.encantamento_name}>Protecao</span>
-                                <span className={styles.encantamento_level}>I</span>
-                            </h2>
-                            <h2 className={styles.encantamento_name_sigla}>Protecao</h2>
+                            <span>
+                                <img src="/Enchanted_Book.webp" alt="encantamento1" width={64} height={64} />
+                            </span>
                         </li>
-
                         <li className={styles.encantamento}>
-                            <h2>
-                                <span className={styles.encantamento_name}>Protecao</span>
-                                <span className={styles.encantamento_level}>I</span>
-                            </h2>
-                            <h2 className={styles.encantamento_name_sigla}>Protecao</h2>
+                            <span>
+                                <img src="/Enchanted_Book.webp" alt="encantamento1" width={64} height={64} />
+                            </span>
                         </li>
-
                         <li className={styles.encantamento}>
-                            <h2>
-                                <img className={styles.encantamento_img} src="/Enchanted_Book.webp" alt="logo" width={64} height={64} />
-                                <span className={styles.encantamento_name}>Protecao</span>
-                                <span className={styles.encantamento_level}>I</span>
-                            </h2>
-                            <h2 className={styles.encantamento_name_sigla}>Protecao</h2>
+                            <span>
+                                <img src="/Enchanted_Book.webp" alt="encantamento1" width={64} height={64} />
+                            </span>
                         </li>
+                        <li className={styles.encantamento}>
+                            <span>
+                                <img src="/Enchanted_Book.webp" alt="encantamento1" width={64} height={64} />
+                            </span>
+                        </li>
+                        <li className={styles.encantamento}>
+                            <span>
+                                <img src="/Enchanted_Book.webp" alt="encantamento1" width={64} height={64} />
+                            </span>
+                        </li>
+                        <li className={styles.encantamento}>
+                            <span>
+                                <img src="/Enchanted_Book.webp" alt="encantamento1" width={64} height={64} />
+                            </span>
+                        </li>
+                        
                     </ul>
                 </nav>
             </div>
