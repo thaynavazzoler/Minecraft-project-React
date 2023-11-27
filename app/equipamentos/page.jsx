ca030d3b360b6c00e2cf5bed584c2dd69fb79a78
pravel <123407140+ElMatheus@users.noreply.github.com>
2023-11-27 11:19:30 -0300

    fit: armadura apareceno

--- app/equipamentos/page.jsx
+++ app/equipamentos/page.jsx
@@ -7,79 +7,83 @@ const equipamentoPage = () => {
     const [equipamento, setEquipamento] = useState(null)
 
     return (
-        <div className={styles.bg}>
+        <>
             <Header />
-            <div className={styles.boxInventory}>
-                <div className={styles.armadura}>
-                    <div onClick={() => setEquipamento('capacete')} className={styles.itemArmadura}></div>
-                    <div onClick={() => setEquipamento('peitoral')} className={styles.itemArmadura}></div>
-                    <div onClick={() => setEquipamento('calca')} className={styles.itemArmadura}></div>
-                    <div onClick={() => setEquipamento('bota')} className={styles.itemArmadura}></div>
-                    <div onClick={() => setEquipamento('espada')} className={styles.itemArmadura}></div>
-                    {
-                        equipamento == 'capacete' ? (
-                            <div>
-                                <h1>Capacete</h1>
-                                <input type="text" placeholder='Nome do equipamento' />
-                                <input type="text" placeholder='Descrição do equipamento' />
-                                <input type="text" placeholder='Material do equipamento' />
-                                <input type="number" placeholder='Valor da defesa' />
-                                <input type="text" placeholder='Cor do equipamento' />
-                            </div>
-                        ) : null
-                    }
-                    {
-                        equipamento == 'peitoral' ? (
-                            <div>
-                                <h1>Peitoral</h1>
-                                <input type="text" placeholder='Nome do equipamento' />
-                                <input type="text" placeholder='Descrição do equipamento' />
-                                <input type="text" placeholder='Material do equipamento' />
-                                <input type="number" placeholder='Valor da defesa' />
-                                <input type="text" placeholder='Cor do equipamento' />
-                            </div>
-                        ) : null
-                    }
-                    {
-                        equipamento == 'calca' ? (
-                            <div>
-                                <h1>Calça</h1>
-                                <input type="text" placeholder='Nome do equipamento' />
-                                <input type="text" placeholder='Descrição do equipamento' />
-                                <input type="text" placeholder='Material do equipamento' />
-                                <input type="number" placeholder='Valor da defesa' />
-                                <input type="text" placeholder='Cor do equipamento' />
-                            </div>
-                        ) : null
-                    }
-                    {
-                        equipamento == 'bota' ? (
-                            <div>
-                                <h1>Bota</h1>
-                                <input type="text" placeholder='Nome do equipamento' />
-                                <input type="text" placeholder='Descrição do equipamento' />
-                                <input type="text" placeholder='Material do equipamento' />
-                                <input type="number" placeholder='Valor da defesa' />
-                                <input type="text" placeholder='Cor do equipamento' />
-                            </div>
+            <div className={styles.bg}>
+                <div className={styles.boxInventory}>
+                    <div className={styles.armadura}>
+                        <div onClick={() => setEquipamento('capacete')} className={styles.itemArmadura}></div>
+                        <div onClick={() => setEquipamento('peitoral')} className={styles.itemArmadura}>
+                            <img className={`${styles.itemDoItem} ${styles.hidden}`} src={"inventory/golden_chestplate.png"} alt="peitoral" />
+                        </div>
+                        <div onClick={() => setEquipamento('calca')} className={styles.itemArmadura}></div>
+                        <div onClick={() => setEquipamento('bota')} className={styles.itemArmadura}></div>
+                        <div onClick={() => setEquipamento('espada')} className={styles.itemArmadura}></div>
+                        {
+                            equipamento == 'capacete' ? (
+                                <div>
+                                    <h1>Capacete</h1>
+                                    <input type="text" placeholder='Nome do equipamento' />
+                                    <input type="text" placeholder='Descrição do equipamento' />
+                                    <input type="text" placeholder='Material do equipamento' />
+                                    <input type="number" placeholder='Valor da defesa' />
+                                    <input type="text" placeholder='Cor do equipamento' />
+                                </div>
+                            ) : null
+                        }
+                        {
+                            equipamento == 'peitoral' ? (
+                                <div>
+                                    <h1>Peitoral</h1>
+                                    <input type="text" placeholder='Nome do equipamento' />
+                                    <input type="text" placeholder='Descrição do equipamento' />
+                                    <input type="text" placeholder='Material do equipamento' />
+                                    <input type="number" placeholder='Valor da defesa' />
+                                    <input type="text" placeholder='Cor do equipamento' />
+                                </div>
+                            ) : null
+                        }
+                        {
+                            equipamento == 'calca' ? (
+                                <div>
+                                    <h1>Calça</h1>
+                                    <input type="text" placeholder='Nome do equipamento' />
+                                    <input type="text" placeholder='Descrição do equipamento' />
+                                    <input type="text" placeholder='Material do equipamento' />
+                                    <input type="number" placeholder='Valor da defesa' />
+                                    <input type="text" placeholder='Cor do equipamento' />
+                                </div>
+                            ) : null
+                        }
+                        {
+                            equipamento == 'bota' ? (
+                                <div>
+                                    <h1>Bota</h1>
+                                    <input type="text" placeholder='Nome do equipamento' />
+                                    <input type="text" placeholder='Descrição do equipamento' />
+                                    <input type="text" placeholder='Material do equipamento' />
+                                    <input type="number" placeholder='Valor da defesa' />
+                                    <input type="text" placeholder='Cor do equipamento' />
+                                </div>
 
-                        ):null
-                    }
-                    {
-                        equipamento == 'espada' ? (
-                            <div>
-                                <h1>Espada</h1>
-                                <input type="text" placeholder='Nome do equipamento' />
-                                <input type="text" placeholder='Descrição do equipamento' />
-                                <input type="text" placeholder='Material do equipamento' />
-                                <input type="number" placeholder='Valor do ataque' />
-                                <input type="text" placeholder='Cor do equipamento' />
-                            </div>
-                        ) : null
-                    }
+                            ) : null
+                        }
+                        {
+                            equipamento == 'espada' ? (
+                                <div>
+                                    <h1>Espada</h1>
+                                    <input type="text" placeholder='Nome do equipamento' />
+                                    <input type="text" placeholder='Descrição do equipamento' />
+                                    <input type="text" placeholder='Material do equipamento' />
+                                    <input type="number" placeholder='Valor do ataque' />
+                                    <input type="text" placeholder='Cor do equipamento' />
+                                </div>
+                            ) : null
+                        }
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
